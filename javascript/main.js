58dfe9689c00709b3b476cf980725c1bc97fbaeb
fgonsebatt <80929108+fgonsebatt@users.noreply.github.com>
2022-10-01 19:46:47 -0300

    head&foot

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+document.getElementById("header").innerHTML = `
+<div class="row">
+    <div class="head">
+        <h1>Jardineria</h1>
+        <div>
+            <div class="col-2 col-s-5 boton"><a href="index.html">Home</a></div>
+            <div class="col-2 col-s-5 boton"><a href="armarjardin.html">Tips Para Jardines</a></div>
+            <div class="col-2 col-s-5 boton"><a href="tienda.html">Tienda</a></div>
+            <div class="col-2 col-s-5 boton"><a href="contacto.html">Contacto</a></div>
+            <div class="col-2 col-s-5 boton"><a href="sobrenosotros.html">Sobre Nosotros</a></div>
+        </div>
+    </div>
+</div>
+
+`
+document.getElementById("footer").innerHTML = `
+<a target="_blank" href="https://www.facebook.com/"><img width="25px" src="./imagenes/facebook.png"
+                alt="Facebook"></a>
+
+        <a target="_blank" href="https://wa.me/+5491136383241?text=Hello,%20please%20send%20me%20info"> <img
+                width="25px" src="./imagenes/whatsapp.png" alt="whatsapp"> </a>
+        <a target="_blank" href="mailto:codoviajes@turismo.com"><img width="25px"
+                src="./imagenes/correo-electronico.png" alt="Mail"></a>
+        <a target="_blank" href="https://www.instagram.com/"><img width="25px" src="./imagenes/instagram.png"
+                alt="Instagram"></a>
+        <p>Pagina creada para el TP </p>
+
+`
