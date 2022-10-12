d1f43174bdc4330581e8806a55a5b10a079b722a
fgonsebatt <80929108+fgonsebatt@users.noreply.github.com>
2022-10-12 10:16:13 -0300

    vueltaAtras

--- javascript/main.js
+++ javascript/main.js
@@ -1,43 +1,39 @@
 document.getElementById("header").innerHTML = `
 <div>
-<div class="head" id="menu-big">
-    <h1 id="jar1">Jardineria</h1>
-    <div>
-        <div class="col-2 col-s-5 boton"><a href="index.html">Home</a></div>
-        <div class="col-2 col-s-5 boton"><a href="armarjardin.html">Tips Para Jardines</a></div>
-        <div class="col-2 col-s-5 boton"><a href="tienda.html">Tienda</a></div>
-        <div class="col-2 col-s-5 boton"><a href="contacto.html">Contacto</a></div>
-        <div class="col-2 col-s-5 boton"><a href="sobrenosotros.html">Sobre Nosotros</a></div>
-    </div>
-</div>
-<div id="menu-drop">
-    <div class="dropdown col-s-2" >
-        <button class="dropbtn"><img src="./imagenes/tree3.jpg" width="100px" alt="tree2"></button>
-        <div class="dropdown-content">
-            <div class="menugrid">
-                <div class="item1"><a href="index.html">Home</a></div>
-                <div class="item2"><a href="armarjardin.html">Tips Para Jardines</a></div>
-                <div class="item3"><a href="tienda.html">Tienda</a></div>
-                <div class="item4"><a href="contacto.html">Contacto</a></div>
-                <div class="item5"><a href="sobrenosotros.html">Sobre Nosotros</a></div>
-                <div class="item6"><img src="./imagenes/root2.jpg" height="200px" alt="root"></div>
+            <div class="head" id="menu-big">
+                <h1 id="jar1">Jardineria</h1>
+                <div class="menugrid-bg">
+                    <div class="col-2 col-s-5 boton"><a href="index.html">Home</a></div>
+                    <div class="col-2 col-s-5 boton"><a href="armarjardin.html">Tips Para Jardines</a></div>
+                    <div class="col-2 col-s-5 boton"><a href="tienda.html">Tienda</a></div>
+                    <div class="col-2 col-s-5 boton"><a href="contacto.html">Contacto</a></div>
+                    <div class="col-2 col-s-5 boton"><a href="sobrenosotros.html">Sobre Nosotros</a></div>
+                </div>
+            </div>
+            <div id="menu-drop">
+                <div class="dropdown col-s-2">
+                    <button class="dropbtn"><img src="./imagenes/tree3.jpg" width="100px" alt="tree2"></button>
+                    <div class="dropdown-content">
+                        <div class="menugrid">
+                            <div class="item1"><a href="index.html">Home</a></div>
+                            <div class="item2"><a href="armarjardin.html">Tips Para Jardines</a></div>
+                            <div class="item3"><a href="tienda.html">Tienda</a></div>
+                            <div class="item4"><a href="contacto.html">Contacto</a></div>
+                            <div class="item5"><a href="sobrenosotros.html">Sobre Nosotros</a></div>
+                            <div class="item6"><img src="./imagenes/root2.jpg" height="200px" alt="root"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-s-10">
+                    <h1 id="jar2">Jardineria</h1>
+                </div>
+                <div class="head-s col-s-12"></div>
             </div>
         </div>
-    </div>
-    <div class="col-s-10">
-        <h1 id="jar2">Jardineria</h1>
-    </div>
-    <div class="head-s col-s-12"></div>
-</div>
-
-
-</div>
-
 `
 document.getElementById("footer").innerHTML = `
 <a target="_blank" href="https://www.facebook.com/"><img width="25px" src="./imagenes/facebook.png"
                 alt="Facebook"></a>
-
         <a target="_blank" href="https://wa.me/+5491136383241?text=Hello,%20please%20send%20me%20info"> <img
                 width="25px" src="./imagenes/whatsapp.png" alt="whatsapp"> </a>
         <a target="_blank" href="mailto:codojardines@plantas.com"><img width="25px"
@@ -45,5 +41,4 @@ document.getElementById("footer").innerHTML = `
         <a target="_blank" href="https://www.instagram.com/"><img width="25px" src="./imagenes/instagram.png"
                 alt="Instagram"></a>
         <p>Pagina creada para el TP </p>
-
 `
