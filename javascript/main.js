ca605efa1ad18b927cf2ae0fa3bf0f6f24a33ccd
fgonsebatt <80929108+fgonsebatt@users.noreply.github.com>
2022-10-23 13:25:40 -0300

    formulario

--- javascript/main.js
+++ javascript/main.js
@@ -44,7 +44,7 @@ document.getElementById("header").innerHTML = `
 document.getElementById("footer").innerHTML = `
 <a target="_blank" href="https://www.facebook.com/"><img width="25px" src="./imagenes/facebook.png"
                 alt="Facebook"></a>
-        <a target="_blank" href="https://wa.me/+5491136383241?text=Hello,%20please%20send%20me%20info"> <img
+        <a target="_blank" href="https://wa.me/+5491112345678?text=Hello,%20please%20send%20me%20info"> <img
                 width="25px" src="./imagenes/whatsapp.png" alt="whatsapp"> </a>
         <a target="_blank" href="mailto:codojardines@plantas.com"><img width="25px"
                 src="./imagenes/correo-electronico.png" alt="Mail"></a>
